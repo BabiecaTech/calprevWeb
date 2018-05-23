5777750746c747acc3d73dc27895f8cb194e9cd6
carser <ascardozo830@gmail.com>
2018-05-23 10:24:56 -0300

    menu de bienvenida

---
+++ menu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<div class="navbar">
+  <div class="navbar-inner">
+	<div class="container">
+	  <div class="nav-collapse">
+		<ul class="nav pull-right">
+				<p>Bienvenido <strong><?php echo $_SESSION['user'];?></strong></p>
+			  <p><a href="desconectar.php"> Cerrar Cesión </a></p>			 
+		</ul>
+	  </div>
+	</div>
+  </div>
+</div>
+</html>
