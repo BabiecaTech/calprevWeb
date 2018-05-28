42ff6fe276ed2564c23f8d3b262464c57b6f5bce
carser <ascardozo830@gmail.com>
2018-05-28 12:09:40 -0300

    modificacion esquema base de datos

--- schema.sql
+++ schema.sql
@@ -10,38 +10,40 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `login`
 --
 
-CREATE TABLE `login` (
-  `id` int(11) NOT NULL,
-  `user` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL,
-  `email` varchar(250) NOT NULL,
-  `pasadmin` varchar(250) NOT NULL,
-  `rol` int(3) NOT NULL
+
+DROP TABLE IF EXISTS login; -- login
+CREATE TABLE login (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  user varchar(50) NOT NULL,
+  password varchar(25) NOT NULL,
+  email varchar(100) NOT NULL,
+  pasadmin varchar(25) NOT NULL,
+  rol int(3) NOT NULL,
+  CONSTRAINT login_pk PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS events; -- events
+CREATE TABLE events (
+  id int(11) NOT NULL,
+  title varchar(200) DEFAULT NULL,
+  color varchar(10) DEFAULT NULL,
+  start date DEFAULT NULL,
+  end date DEFAULT NULL,
+  id_user int(11) NOT NULL,
+  CONSTRAINT events_pk PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE events ADD FOREIGN KEY(id_user) REFERENCES login(id);
+
 --
--- Volcado de datos para la tabla `login`
+-- insertar datos
 --
 
-INSERT INTO `login` (`id`, `user`, `password`, `email`, `pasadmin`, `rol`) VALUES
+INSERT INTO login (id, user, password, email, pasadmin, rol) VALUES
 (1, 'Administrador', '', 'admin@gmail.com', '123456', 1),
 (2, 'Sergio', '12345', 'sergio@gmail.com', '', 2),
+(3, 'Federico', 'gimli777', 'fedeaguirre@gmail.com', '', 2);
 
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `login`
---
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `login`
---
-ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+INSERT INTO events (id, title, color, start, end, id_user) VALUES
+(1, 'Tarea 1', '#167EAF', '2018-05-09', '2018-05-15', 3),
+(2, 'Tarea 1', '#167EAF', '2018-05-09', '2018-05-12', 2);
