f09ba5de7dd05c35f72a13bb0af1e2bf398902b6
carser <ascardozo830@gmail.com>
2018-06-07 00:51:38 -0300

    optimizaciion archivo validar

--- validar.php
+++ validar.php
@@ -12,23 +12,14 @@ session_start();
 			$_SESSION['id']=$f2['id'];
 			$_SESSION['user']=$f2['user'];
 			$_SESSION['rol']=$f2['rol'];
-
-			echo '<script>alert("BIENVENIDO ADMINISTRADOR")</script> ';
-			echo "<script>location.href='admin.php'</script>";
-		
-		}
-	}
-
-
-	$sql=mysqli_query($conn,"SELECT * FROM login WHERE email='$username'");
-	if($f=mysqli_fetch_assoc($sql)){
-		if($pass==$f['password']){
-			$_SESSION['id']=$f['id'];
-			$_SESSION['user']=$f['user'];
-			$_SESSION['rol']=$f['rol'];
+			header("Location: lista_usuarios.php");
+		}else if($pass==$f2['password']){
+			$_SESSION['id']=$f2['id'];
+			$_SESSION['user']=$f2['user'];
+			$_SESSION['rol']=$f2['rol'];
 
 			header("Location: calendario.php");
-		}else{
+		}else {
 			echo '<script>alert("CONTRASEÑA INCORRECTA")</script> ';
 		
 			echo "<script>location.href='index.php'</script>";
